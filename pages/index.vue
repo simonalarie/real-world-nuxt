75fc1591fa4f8e46cbce946346f0752935f11d6d
Alarie Simon <simon.alarie@loto-quebec.com>
2022-07-19 16:29:08 -0400

    Adding SEO

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
- <template>
-      <div>
-        <h1>Events</h1>
-      </div>
-    </template>
+<template>
+    <div>
+      <h1>Events</h1>
+    </div>
+</template>
+<script>
+    export default {
+      head() { // <-- property used by vue-meta to add header tags
+        return {
+          title: 'Event Listing - Real World Events', // <-- For our title tag
+          meta: [
+            {
+              hid: 'description',  
+              name: 'description', // <-- for our meta description tag
+              content:
+                'Where you can find all the events taking place in your neighborhood'
+            }
+          ]
+        }
+      }
+    }
+</script>
