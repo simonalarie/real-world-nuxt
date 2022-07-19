8d39118e176c24661b53e3ee455b51f2d33ec25c
Alarie Simon <simon.alarie@loto-quebec.com>
2022-07-19 17:07:52 -0400

    Add file based routing

---
+++ pages/event/_id.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+    <h1>Individual Event: {{ id }}</h1>
+    </div>
+</template>
+<script>
+    export default {
+    // SEO vue-meta
+      head() {
+        return {
+          title: 'Event #' + this.id,
+          meta: [
+            {
+              hid: 'description',
+              name: 'description',
+              content: 'What you need to know about Event #' + this.id
+            }
+          ]
+        }
+      },
+      computed: {
+        id() {
+          return this.$route.params.id
+        }
+      }
+    }
+    </script>
