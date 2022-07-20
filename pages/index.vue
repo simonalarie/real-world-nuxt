eee6961baf073395af7b169d84dbbc7df985cd03
Alarie Simon <simon.alarie@loto-quebec.com>
2022-07-20 17:10:27 -0400

    axios calls done

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
     <div>
       <h1>Events</h1>
+      <EventCard
+        v-for="(event, index) in events"
+        :key="index"
+        :event="event"
+        :data-index="index"
+      />
     </div>
 </template>
 <script>
+    import EventCard from '@/components/EventCard.vue'
     export default {
       head() { // <-- property used by vue-meta to add header tags
         return {
@@ -17,6 +24,22 @@
             }
           ]
         }
+      },
+      // asyncData is called every time before loading a page component (pages dir.)
+      // Receives context: used to fetch some data (via API) and then Nuxt will merge the return value with component's data.
+      asyncData(context) {
+        // Nuxt wait for promise to be resolved before rendering component
+        return context.$axios.get('http://localhost:3000/events').then(response => {
+          // Je retourne an events object
+          return {
+            events: response.data
+          }
+        }).catch(e => {
+          context.error({ statusCode: 503, message: 'Unable to fetch events at this time, please try again' })
+        })
+      },
+      components: {
+        EventCard
       }
     }
 </script>
