0f625e3ebe527864db7f8ba018b5d993ea32ca54
Alarie Simon <simon.alarie@loto-quebec.com>
2022-07-21 15:32:53 -0400

    ready to deploy

--- services/EventService.js
+++ services/EventService.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  // baseURL: `http://localhost:3000`,
-  baseURL: `https://my-json-server.typicode.com/Code-Pop/real-world-nuxt`,
+  baseURL: `http://localhost:3000`,
+  // baseURL: `https://my-json-server.typicode.com/Code-Pop/real-world-nuxt`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
